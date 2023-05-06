4d1c9a5861185f8d462f26716ea01d216f4e1c76
Severuna <oxxxyfomenkova@gmail.com>
2023-05-06 22:31:09 +0300

    create Main component & import

---
+++ src/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <div className="main flex-row">
+            <h1 className="title">Init title</h1>
+        </div>
+    )
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import Main from './Main';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1 className='title'>Homework #19. ReactJS. Поняття SPA. Навігація</h1>
+  <Main />
 );
