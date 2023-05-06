e7e75ce46fd2bbdf6b2cce6a7db2755627dff39a
Severuna <oxxxyfomenkova@gmail.com>
2023-05-07 00:40:58 +0300

    create navbar, pages & routes

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,23 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './Main.css';
+import NavBar from './components/nav-bar/NavBar';
+import Home from './pages/home/Home';
+import Activities from './pages/activities/Activities';
+import Contacts from './pages/contacts/Contacts';
+import Works from './pages/works/Works';
 
 const Main = () => {
     return (
         <div className="main flex-column">
-            <nav className="header flex-row">
-                <a className='header-item'>R-n-W</a>
-                <a className='header-item'>Activities</a>
-                <a className='header-item'>Works</a>
-                <a className='header-item'>Contacts</a>
-            </nav>
+            <BrowserRouter>
+                <NavBar />
+                        <Routes>
+                        <Route path='/react_spa/activities' element={<Activities />} />
+                        <Route path='/react_spa/contacts' element={<Contacts />} />
+                        <Route path='/react_spa/works' element={<Works />} />
+                        <Route path='/react_spa/' element={<Home />} />
+                        </Routes>
+            </BrowserRouter>
         </div>
     )
 }
---
+++ src/components/nav-bar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+
+    useEffect(() => {
+        console.log('page load');
+    }, []);
+    
+    return (
+        <nav className="header flex-row">
+            <Link to='react_spa' className='header-item'>R-n-W</Link>
+            <Link to='react_spa/activities' className='header-item'>Activities</Link>
+            <Link to='react_spa/works' className='header-item'>Works</Link>
+            <Link to='react_spa/contacts' className='header-item'>Contacts</Link>
+        </nav>
+    );
+};
+
+export default NavBar;
