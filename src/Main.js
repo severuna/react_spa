bc3798371043bc174e30a3389592cc390cf77a61
Severuna <oxxxyfomenkova@gmail.com>
2023-05-06 23:01:08 +0300

    add init nav mark & styles

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,14 @@
+import './Main.css';
+
 const Main = () => {
     return (
-        <div className="main flex-row">
-            <h1 className="title">Init title</h1>
+        <div className="main flex-column">
+            <nav className="header flex-row">
+                <a className='header-item'>R-n-W</a>
+                <a className='header-item'>Activities</a>
+                <a className='header-item'>Works</a>
+                <a className='header-item'>Contacts</a>
+            </nav>
         </div>
     )
 }
